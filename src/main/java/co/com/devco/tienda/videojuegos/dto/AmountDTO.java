32762288acc2cf7986c1a78e896f242863e14db0
Jia Chen <jiamchenchen@gmail.com>
2024-09-19 22:52:51 -0500

    pruebas unitarias y cobertura

---
+++ src/main/java/co/com/devco/tienda/videojuegos/dto/AmountDTO.java
@@ -0,0 +1,18 @@
+package co.com.devco.tienda.videojuegos.dto;
+
+public class Amount {
+
+    private int amount;
+
+    public Amount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
